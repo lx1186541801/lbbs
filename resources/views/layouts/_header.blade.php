dc7f40325398ea09f176f8d8b2fc8c49ecb7d4b0
Robert.LJ <1186541801@qq.com>
2021-05-31 08:06:14 +0000

    生成用户认证代码

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -15,8 +15,8 @@
 
 			{{-- right side --}}
 			<ul class="navbar-nav navbar-right">
-				<li class="nav-item"><a href="#" class="nav-link">登录</a></li>
-				<li class="nav-item"><a href="#" class="nav-link">注册</a></li>
+				<li class="nav-item"><a href="{{ route('login') }}" class="nav-link">登录</a></li>
+				<li class="nav-item"><a href="{{ route('register') }}" class="nav-link">注册</a></li>
 			</ul>
 		</div>
 	</div>
