0330137319b220e5b6402d63c0245685020742b4
Robert.LJ <1186541801@qq.com>
2021-06-11 03:29:19 +0000

    add 头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -5,34 +5,42 @@ namespace App\Handlers;
 use Illuminate\Support\Str;
 
 /**
- * 	
+ * 	图片上传类
  */
+
 class ImageUploadHandler 
 {
 
+	// 允许上传的类型文件
 	protected $allowed_ext = ['png', 'jpg', 'gif', 'jpeg'];
 
 
 	public function save($file, $folder, $file_prefix)
 	{
+		// 存储目录
 		$folder_name = "uploads/images/$folder/" . date('Ym/d', time());
 
+		// 物理目录
+		// public_path()` 获取的是 `public` 文件夹的物理路径。
+		$uploads_path = public_path() . '/' . $folder_name;
 
-		$uploads_path = public_path() . '' . $folder_name;
+		// 文件后缀
+		$ext = strtolower($file->getClientOriginalExtension()) ?: 'png';
 
-		$ext = strtolower($file->getClientOriginalExtendsion()) ?: 'png';
+		// 文件名称
+		$filename = $file_prefix . '_' . date('Y_m_d_H_i_s', time()) . '_' . Str::random(10) . '.' . $ext;
 
-		$filename = $file_prefix . '_' . time() . '_' . Str::random(10) . '.' . $ext;
-
-
+		// 是否是允许的文件后缀
 		if ( ! in_array($ext, $this->allowed_ext)) {
 			return false;
 		}
 
+		// 移动文件到指定的文件目录
 		$file->move($uploads_path, $filename);
 
+		// 返回文件全路径
 		return [
-			'path' => config('app.url') . "/$uploads_path/$filename"
+			'path' => config('app.url') . "/$folder_name/$filename"
 		];
 	}
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 
 
@@ -21,11 +22,17 @@ class UsersController extends Controller
     	return  view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, User $user, ImageUploadHandler $uploader)
     {
 
-    	dd($request->avatar);
-    	$user->update($request->all());
+    	$data = $request->all();
+
+    	$result = $uploader->save($request->avatar, 'avatars', $user->id);
+    	if ($result) {
+    		$data['avatar'] = $result['path'];
+    	}
+
+    	$user->update($data);
 
     	return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
