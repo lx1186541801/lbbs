8c5615f1643f210f3d9757d8951a23ebb9e6911e
Robert.LJ <1186541801@qq.com>
2021-06-21 01:15:11 +0000

    裁剪图片

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -2,7 +2,9 @@
 
 namespace App\Handlers;
 
-use Illuminate\Support\Str;
+use Str;
+use Image;
+
 
 /**
  * 	图片上传类
@@ -15,7 +17,7 @@ class ImageUploadHandler
 	protected $allowed_ext = ['png', 'jpg', 'gif', 'jpeg'];
 
 
-	public function save($file, $folder, $file_prefix)
+	public function save($file, $folder, $file_prefix, $max_width = false)
 	{
 		// 存储目录
 		$folder_name = "uploads/images/$folder/" . date('Ym/d', time());
@@ -28,7 +30,7 @@ class ImageUploadHandler
 		$ext = strtolower($file->getClientOriginalExtension()) ?: 'png';
 
 		// 文件名称
-		$filename = $file_prefix . '_' . date('Y_m_d_H_i_s', time()) . '_' . Str::random(10) . '.' . $ext;
+		$filename = $file_prefix . '_' . time() . '_' . Str::random(10) . '.' . $ext;
 
 		// 是否是允许的文件后缀
 		if ( ! in_array($ext, $this->allowed_ext)) {
@@ -38,9 +40,36 @@ class ImageUploadHandler
 		// 移动文件到指定的文件目录
 		$file->move($uploads_path, $filename);
 
+
+		if ($max_width && $ext != 'gif') {
+			$this->reduceSize($uploads_path . '/' . $filename, $max_width);
+		}
+
 		// 返回文件全路径
 		return [
 			'path' => config('app.url') . "/$folder_name/$filename"
 		];
+	}
+
+
+	public function reduceSize($file_path, $max_width)
+	{
+		// 获取对象，参数为图片的物理路径
+		$image = Image::make($file_path);
+
+		// 进行大小调整
+		$image->resize($max_width, null, function ($constraint) {
+
+			// 设置宽度为$max_width 等比例缩放
+			$constraint->aspectRatio();
+
+			// 防止裁图时图片尺寸变大
+			$constraint->upsize();
+
+		});
+
+		// 保存
+		$image->save();
+
 	}
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -27,7 +27,7 @@ class UsersController extends Controller
 
     	$data = $request->all();
 
-    	$result = $uploader->save($request->avatar, 'avatars', $user->id);
+    	$result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
     	if ($result) {
     		$data['avatar'] = $result['path'];
     	}
