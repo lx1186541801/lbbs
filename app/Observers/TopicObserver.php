36d231610bc8a5f18e6dae39cd259d75824f3498
Robert.LJ <1186541801@qq.com>
2021-07-01 08:53:11 +0000

    XSS注入防范

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -10,6 +10,13 @@ class TopicObserver
     //
 	public function saving(Topic $topic)
 	{
+
+		// 过滤body  防止 XSS 攻击
+		$topic->body = clean($topic->body, 'user_topic_body');
+
 		$topic->excerpt = make_excerpt($topic->body);
 	}
+
+
+
 }
